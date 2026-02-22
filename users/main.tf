add7602936d7caeeea818127eca708b55100b43d
ravi-burman <248809268+atharvYadavXperate@users.noreply.github.com>
2026-02-22 17:09:14 +0530

    tf and ymal update

---
+++ users/main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "project_number" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "service_account" {
+  type = string
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+resource "google_storage_bucket" "source_bucket" {
+  name                        = "${var.project_id}-function-${random_id.bucket_suffix.hex}"
+  location                    = var.region
+  uniform_bucket_level_access = true
+  force_destroy               = true
+}
+
+resource "google_storage_bucket_object" "source_archive" {
+  name   = "source.zip"
+  bucket = google_storage_bucket.source_bucket.name
+  source = "source.zip"
+}
+
+resource "google_cloudfunctions2_function" "users" {
+  name     = "users"
+  location = var.region
+
+  build_config {
+    runtime     = "go121"
+    entry_point = "usersthHTTP"
+
+    source {
+      storage_source {
+        bucket = google_storage_bucket.source_bucket.name
+        object = google_storage_bucket_object.source_archive.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count    = 1
+    available_memory      = "256M"
+    timeout_seconds       = 60
+    service_account_email = var.service_account
+    ingress_settings      = "ALLOW_ALL"
+  }
+}
+
+resource "google_cloudfunctions2_function_iam_member" "public_access" {
+  project        = var.project_id
+  location       = var.region
+  cloud_function = google_cloudfunctions2_function.users.name
+  role           = "roles/run.invoker"
+  member         = "allUsers"
+}
+
+output "function_url" {
+  value = google_cloudfunctions2_function.users.service_config[0].uri
+}
