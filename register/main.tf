c38167e33a4b5a9c6701e033247b6996c2018581
ravi-burman <248809268+atharvYadavXperate@users.noreply.github.com>
2026-02-23 23:02:32 +0530

    public access

--- register/main.tf
+++ register/main.tf
@@ -78,14 +78,15 @@ resource "google_cloudfunctions2_function" "Register" {
   }
 }
 
-resource "google_cloudfunctions2_function_iam_binding" "public_access" {
-  project        = var.project_id
-  cloud_function = google_cloudfunctions2_function.Register.name
-  role           = "roles/cloudfunctions.invoker"
-  members        = ["allUsers"]
+resource "google_cloud_run_service_iam_member" "public_access" {
+  location = var.region
+  service  = google_cloudfunctions2_function.Register.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 
   depends_on = [google_cloudfunctions2_function.Register]
 }
+
 output "function_url" {
   value = google_cloudfunctions2_function.Register.service_config[0].uri
 }
