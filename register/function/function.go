a08789da306dec20b4b7a7b2587089c8542ff962
ravi-burman <248809268+atharvYadavXperate@users.noreply.github.com>
2026-02-23 23:36:24 +0530

    refister update

--- register/function/function.go
+++ register/function/function.go
@@ -2,18 +2,28 @@ package authfunction
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/atharvYadavXperate/newCicd/kapad-app/database/users"
 )
 
 func Register(w http.ResponseWriter, r *http.Request) {
-	var user = users.UserSchema{}
-	user.ParseData(r.Body)
-	w.Header().Set("Content-type", "application/json")
-	json, err := user.ToJSON()
+	var user users.UserSchema
+
+	if err := user.ParseData(r.Body); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	user.Role = "user"
+	user.Verified = false
+	user.CreatedAt = time.Now()
+	w.Header().Set("Content-Type", "application/json")
+
+	jsonData, err := user.ToJSON()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Write(json)
+
+	w.Write(jsonData)
 }
--- register/function/go.mod
+++ register/function/go.mod
@@ -2,4 +2,4 @@ module github.com/atharvYadavXperate/newCicd/register
 
 go 1.25.5
 
-require github.com/atharvYadavXperate/newCicd v0.0.0-20260223172412-8016a49500ad
+require github.com/atharvYadavXperate/newCicd v0.0.0-20260223175157-556923031086
