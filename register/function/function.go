fcfc713b2117d4248e6573e13d59358b5b8c2923
ravi-burman <248809268+atharvYadavXperate@users.noreply.github.com>
2026-02-24 11:44:29 +0530

    update regsite fucntion

--- register/function/function.go
+++ register/function/function.go
@@ -5,16 +5,26 @@ import (
 	"time"
 
 	"github.com/atharvYadavXperate/newCicd/kapad-app/database/users"
+	customerror "github.com/atharvYadavXperate/newCicd/kapad-app/domain/errors"
 )
 
 func Register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, customerror.ErrMethodNotAllows.Error(), http.StatusBadRequest)
+		return
+	}
 	var user users.UserSchema
 
 	if err := user.ParseData(r.Body); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, customerror.ErrParseError.Error(), http.StatusBadRequest)
 		return
 	}
-	user.Role = "user"
+
+	if user.IsAllRequiredFields() {
+		http.Error(w, "All fields are required", http.StatusBadRequest)
+		return
+	}
+
 	user.Verified = false
 	user.CreatedAt = time.Now()
 	w.Header().Set("Content-Type", "application/json")
--- register/function/go.mod
+++ register/function/go.mod
@@ -2,4 +2,4 @@ module github.com/atharvYadavXperate/newCicd/register
 
 go 1.25.5
 
-require github.com/atharvYadavXperate/newCicd v0.0.0-20260223175157-556923031086
+require github.com/atharvYadavXperate/newCicd v0.0.0-20260224043709-db262ec0ebe2
