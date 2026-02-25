61f391f1e83e9a6e7b593edc802f2b5c1db51ac7
ravi-burman <248809268+atharvYadavXperate@users.noreply.github.com>
2026-02-25 11:34:03 +0530

    Initlization of the database

--- kapad-app/app/init.go
+++ kapad-app/app/init.go
@@ -1,6 +1,11 @@
 package app
 
-import "os"
+import (
+	"context"
+	"os"
+
+	db "github.com/atharvYadavXperate/newCicd/kapad-app/firestore"
+)
 
 func (a *App) Init() {
 	a.ProjectId = os.Getenv("PROJECT_ID")
@@ -8,4 +13,13 @@ func (a *App) Init() {
 
 func (a *App) GetProjectId() string {
 	return a.ProjectId
+}
+
+func (a *App) InitDatabase() error {
+	database, err := db.NewDatabase(context.Background(), a.ProjectId)
+	if err != nil {
+		return err
+	}
+	a.Database = database
+	return nil
 }
--- kapad-app/app/module.go
+++ kapad-app/app/module.go
@@ -1,5 +1,8 @@
 package app
 
+import db "github.com/atharvYadavXperate/newCicd/kapad-app/firestore"
+
 type App struct {
 	ProjectId string
+	Database  *db.Database
 }
